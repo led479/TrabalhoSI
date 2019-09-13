5ab298433929f3a5cfa3e2f010b23253912eed5b
Rolf Zambon <=>
2019-09-13 17:26:51 -0300

    Inicio da implementação dos filhos

--- classes/estado.py
+++ classes/estado.py
@@ -1,4 +1,6 @@
 from .gerador_de_matriz import GeradorDeMatriz
+from copy import copy, deepcopy
+
 
 class Estado:
     def __init__(self):
@@ -9,6 +11,40 @@ class Estado:
 
         # TODO Adicionar algoritmo para heurística
         self.heuristica = None
+
+    # TODO Já existem as posições por volta do espaço vazio, só falta fazer o método
+    def filhos(self):
+        posicao_vazio = self.indice_do_vazio()
+
+        posicao_cima = [posicao_vazio[0] - 1, posicao_vazio[1]]
+        posicao_direita = [posicao_vazio[0], posicao_vazio[1] + 1]
+        posicao_baixo = [posicao_vazio[0] + 1, posicao_vazio[1]]
+        posicao_esquerda = [posicao_vazio[0], posicao_vazio[1] - 1]
+
+        return [posicao_cima,
+                posicao_direita,
+                posicao_baixo,
+                posicao_esquerda]
+        #if(posicao_vazio == [0, 0]):
+         #   filho1 = deepcopy(self.matriz)
+          #  filho1[0][0] = filho1[0][0 + 1]
+           # filho1[0][0 + 1] = None
+#
+ #           filho2 = deepcopy(self.matriz)
+  #          filho2[0][0] = filho2[0 + 1][0]
+   #        filho2[0 + 1][0] = None
+    #        return [filho1, filho2]
+    #    else:
+     #     return 'eae'
+
+
+
+    def indice_do_vazio(self):
+        for i in range(len(self.matriz)):
+              for j in range(len(self.matriz[i])):
+                  if(self.matriz[i][j] == None):
+                      return [i, j]
+        return []
 
     @staticmethod
     def estado_final():
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from classes.estado import Estado
 
 e = Estado()
-print(e.matriz)
+print(e.matriz[0])
+print(e.matriz[1])
+print(e.matriz[2])
+print(e.indice_do_vazio())
+print(e.filhos())
