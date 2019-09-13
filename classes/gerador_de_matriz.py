75ac1588b4960bb6572062585ab5b323905e0704
Rolf Zambon <=>
2019-09-13 16:06:00 -0300

    Melhora ainda mais o gerador

--- classes/gerador_de_matriz.py
+++ classes/gerador_de_matriz.py
@@ -2,13 +2,13 @@ import random
 
 class GeradorDeMatriz:
     def __init__(self):
-        self.numeros_disponiveis = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.numeros_disponiveis = [1, 2, 3, 4, 5, 6, 7, 8, None]
         random.shuffle(self.numeros_disponiveis)
 
     # Retorna uma matriz com 8 números entre 1 e 8, e um espaço vazio (None)
     # Ex:   [   [5, 3, 4],
     #           [2, None, 7],
-    #           [1, 6, 8] 
+    #           [1, 6, 8]
     #       ]
     def gera_matriz_aleatoria(self):
         m = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -16,11 +16,5 @@ class GeradorDeMatriz:
         for i in range(len(m)):
             for j in range(len(m[i])):
                 m[i][j] = self.numeros_disponiveis.pop()
-
-        # Troca o 9 por None, que representará o estado vazio
-        for i in range(len(m)):
-            for j in range(len(m[i])):
-                if m[i][j] == 9:
-                    m[i][j] = None
 
         return m
